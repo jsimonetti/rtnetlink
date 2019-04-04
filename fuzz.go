ce048b06c7094d6c31ef0ff38edd83fa27843685
Jeroen Simonetti <jeroen@simonetti.nl>
2019-04-04 13:17:31 +0200

    Update fuzzing for RouteMessage and add convenience Makefile

--- fuzz.go
+++ fuzz.go
@@ -5,6 +5,7 @@ package rtnetlink
 func Fuzz(data []byte) int {
 	//return fuzzLinkMessage(data)
 	return fuzzAddressMessage(data)
+	//return fuzzRouteMessage(data)
 }
 
 func fuzzLinkMessage(data []byte) int {
@@ -22,6 +23,19 @@ func fuzzLinkMessage(data []byte) int {
 
 func fuzzAddressMessage(data []byte) int {
 	m := &AddressMessage{}
+	if err := (m).UnmarshalBinary(data); err != nil {
+		return 0
+	}
+
+	if _, err := m.MarshalBinary(); err != nil {
+		panic(err)
+	}
+
+	return 1
+}
+
+func fuzzRouteMessage(data []byte) int {
+	m := &RouteMessage{}
 	if err := (m).UnmarshalBinary(data); err != nil {
 		return 0
 	}
